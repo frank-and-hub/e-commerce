a2d58996f0aeab6b65620c1724a07c58f7e4628d
frank-and-hub <sourabbiswas000x@gmail.com>
2025-02-13 22:41:46 +0530

    added some front-panel files

---
+++ front-panel/src/components/Front/CategoriesofTheMonth/CategoriesofTheMonth.jsx
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class CategoriesofTheMonth extends Component {
+    render() {
+        return (
+            <>
+                <section className="container py-5">
+                    <div className="row text-center pt-3">
+                        <div className="col-lg-6 m-auto">
+                            <h1 className="h1">Categories of The Month</h1>
+                            <p>
+                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                deserunt mollit anim id est laborum.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-md-4 p-5 mt-3">
+                            <Link to="#">
+                                <img src="./assets/img/category_img_01.jpg" className="rounded-circle img-fluid border" />
+                            </Link>
+                            <h5 className="text-center mt-3 mb-3">Watches</h5>
+                            <p className="text-center">
+                                <Link className="btn btn-success">Go Shop
+                                </Link>
+                            </p>
+                        </div>
+                        <div className="col-12 col-md-4 p-5 mt-3">
+                            <Link to="#">
+                                <img src="./assets/img/category_img_02.jpg" className="rounded-circle img-fluid border" />
+                            </Link>
+                            <h2 className="h5 text-center mt-3 mb-3">Shoes</h2>
+                            <p className="text-center"><Link className="btn btn-success">Go Shop</Link></p>
+                        </div>
+                        <div className="col-12 col-md-4 p-5 mt-3">
+                            <Link to="#"
+                            ><img src="./assets/img/category_img_03.jpg" className="rounded-circle img-fluid border" />
+                            </Link>
+                            <h2 className="h5 text-center mt-3 mb-3">Accessories</h2>
+                            <p className="text-center"><Link className="btn btn-success">Go Shop</Link></p>
+                        </div>
+                    </div>
+                </section>
+            </>
+        );
+    }
+}
+
+export default CategoriesofTheMonth;
--- front-panel/src/components/Front/Index/FrontIndex.jsx
+++ front-panel/src/components/Front/Index/FrontIndex.jsx
@@ -1,32 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { get } from '../../../utils/AxiosUtils'
+import React, { useEffect, useState } from 'react'
+// import { get } from '../../../utils/AxiosUtils'
 
-import { Preloader } from '../Preloader/Preloader'
-import { Navigation } from '../Navigation/Navigation'
-import { Slider } from '../Slider/Slider'
-import { About } from '../About/About'
-import { Service } from '../Service/Service'
-import { Work } from '../Work/Work'
-import { Testimonial } from '../Testimonial/Testimonial'
-import { ContactUs } from '../Contact/ContactUs'
-import { Footer } from '../Footer/Footer'
-import { UserModel } from './UserModel';
-// import { Pricing } from '../Pricing/Pricing'
-
+import Navbar from '../Navbar/Navbar'
+import Header from '../Header/Header'
+import Model from '../Modal/Modal'
+import Carousel from '../Carousel/Carousel'
+import CategoriesofTheMonth from '../CategoriesofTheMonth/CategoriesofTheMonth'
+import FeaturedProduct from '../FeaturedProduct/FeaturedProduct'
+import FrontFooter from '../FrontFooter/FrontFooter'
 
 function FrontIndex() {
-    const [user, setUser] = useState(null);
-    const [number, setNumber] = useState(null);
     const [loading, setLoading] = useState(null);
 
     useEffect(() => {
         setLoading(true)
         const fetchData = async () => {
             try {
-                const [userData] = await Promise.all([
-                    get(`/user_details/${number}`),
-                ]);
-                setUser(userData?.data);
+                // const [userData] = await Promise.all([
+                //     get(`/user_details/${number}`),
+                // ]);
                 setLoading(false)
             } catch (err) {
                 setLoading(true)
@@ -34,31 +26,27 @@ function FrontIndex() {
             }
         };
 
-        if (number) {
-            fetchData();
-        }
-    }, [number]);
+        // if (number) {
+        fetchData();
+        // }
+    }, []);
 
     return (
         <>
-            {!number ? (
-                <UserModel setNumber={setNumber} />
-            ) :
-                (loading ? (
-                    <Preloader />
-                ) : (
-                    <>
-                        <Navigation data={user} ></Navigation>
-                        <Slider data={user} ></Slider>
-                        <About data={user} ></About>
-                        <Service data={user} ></Service>
-                        <Work data={user} ></Work>
-                        <Testimonial data={user} ></Testimonial>
-                        {/* <Pricing data={user} ></Pricing> */}
-                        <ContactUs data={user} ></ContactUs>
-                        <Footer data={user} ></Footer>
-                    </>
-                ))}
+            {loading ? (
+                <>
+                </>
+            ) : (
+                <>
+                    <Navbar />
+                    <Header />
+                    <Model />
+                    <Carousel />
+                    <CategoriesofTheMonth />
+                    <FeaturedProduct />
+                    <FrontFooter />
+                </>
+            )}
         </>
     );
 };

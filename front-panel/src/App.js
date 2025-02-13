b77110bd2c79f53d808f31d148177f68fe35ecdb
frank-and-hub <sourabbiswas000x@gmail.com>
2025-02-13 18:39:16 +0530

    update front end data

--- front-panel/src/App.js
+++ front-panel/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import 'react-toastify/dist/ReactToastify.css'
+import Routing from './components/Routing'
+import Notification from './components/Admin/Comman/Notification/Notification'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App`}>
+      <Notification />
+      <Routing />
     </div>
   );
 }
